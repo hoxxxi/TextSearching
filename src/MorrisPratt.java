40213fc2a88c4f5272ede51839d6aa98f8f4f5a9
Yordan <hoxxxii@gmail.com>
2016-05-15 11:44:38 +0100

    clean and rename

--- src/MorrisPratt.java
+++ src/MorrisPratt.java
@@ -1,5 +1,5 @@
 
-public class MorisPratt {
+public class MorrisPratt {
 
 	static int []MP_next = {-1,0,1,2,0,1,2};
 
@@ -7,7 +7,7 @@ public class MorisPratt {
 	{
 		//0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7
 		//A A B A A A A B A B A A A B A A B B
-		//                    A A A B A A
+		//A A A B A A
 		String y = "aabaaaababaaabaabb";
 		int n = y.length();
 		
@@ -28,11 +28,11 @@ public class MorisPratt {
 					k++;
 			}
 			if(k==m)
-				System.out.print("\tComplete match!");
+				System.out.print("\tComplete match at: "+i+"!");
 			
 			int period = k - MP_next[k];
 			i=i + period;
-			System.out.print("\tNo. push: "+period);
+			System.out.print("\tpush: "+period);
 			
 			k = MP_next[k];
 			if (k<0) k = 0;
