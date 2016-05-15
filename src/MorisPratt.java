3187a5f95a42eb60631267e97768473fbbe76b81
Yordan <hoxxxii@gmail.com>
2016-05-15 11:19:40 +0100

    correct jump if match + simplify

--- src/MorisPratt.java
+++ src/MorisPratt.java
@@ -7,41 +7,35 @@ public class MorisPratt {
 	{
 		//0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7
 		//A A B A A A A B A B A A A B A A B B
-		//A A A B A A
+		//                    A A A B A A
 		String y = "aabaaaababaaabaabb";
 		int n = y.length();
 		
 		String x = "aaabaa";
 		int m = x.length();
 
-//		calculate(x,y,m,n);
 		calculateDachiStyle(x,y,m,n);
 	}
 	private static void calculateDachiStyle(String x, String y, int m, int n)
 	{
 		int k = 0;
-		for(int i = 0; i<=n-m;)
+		int i = 0;
+		while (i<=n-m)
 		{
-			int period = 1;
-			for(; k<m; k++)
+			while(k<m && x.charAt(k) == y.charAt(i+k))
 			{
-				if(x.charAt(k) == y.charAt(i+k))
-				{
 					System.out.print("\tMatch: "+x.charAt(k));
-					if(k+1==m)
-						System.out.print("\tComplete match!");
-				}
-				else
-				{
-					period = k - MP_next[k];
-					
-					System.out.print("\tNo. push: "+period);
-					break;
-				}
+					k++;
 			}
+			if(k==m)
+				System.out.print("\tComplete match!");
+			
+			int period = k - MP_next[k];
+			i=i + period;
+			System.out.print("\tNo. push: "+period);
+			
 			k = MP_next[k];
 			if (k<0) k = 0;
-			i+=period;
 			System.out.println();
 		}
 	}
